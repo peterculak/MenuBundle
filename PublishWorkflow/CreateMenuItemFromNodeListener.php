5e72e4ef5751bd06becac9356dbdd4fd5e92aa1c
David Buchmann <david.buchmann@liip.ch>
2014-03-09 22:17:31 +0100

    cleanup the menu creation event and improve doc

--- PublishWorkflow/CreateMenuItemFromNodeListener.php
+++ PublishWorkflow/CreateMenuItemFromNodeListener.php
@@ -5,42 +5,54 @@ namespace Symfony\Cmf\Bundle\MenuBundle\PublishWorkflow;
 use Symfony\Cmf\Bundle\MenuBundle\Event\CreateMenuItemFromNodeEvent;
 use Symfony\Cmf\Bundle\CoreBundle\PublishWorkflow\PublishWorkflowChecker;
 use Symfony\Component\Security\Core\SecurityContextInterface;
-use Symfony\Cmf\Bundle\MenuBundle\Voter\VoterInterface;
 use Symfony\Cmf\Bundle\MenuBundle\Model\Menu;
 
+/**
+ * Listener for the CREATE_ITEM_FROM_NODE event that skips the node if it is
+ * not published.
+ *
+ * @author Ben Glassman <bglassman@gmail.com>
+ */
 class CreateMenuItemFromNodeListener
 {
     /**
      * @var SecurityContextInterface
      */
-    private $securityContext;
+    private $publishWorkflowChecker;
 
     /**
      * The permission to check for when doing the publish workflow check.
      *
      * @var string
      */
-    private $publishWorkflowPermission = PublishWorkflowChecker::VIEW_ATTRIBUTE;
+    private $publishWorkflowPermission;
 
-    public function __construct(SecurityContextInterface $securityContext)
+    /**
+     * @param SecurityContextInterface $publishWorkflowChecker
+     * @param string                   $attribute              the permission to check.
+     */
+    public function __construct(SecurityContextInterface $publishWorkflowChecker, $attribute = PublishWorkflowChecker::VIEW_ATTRIBUTE)
     {
-        $this->securityContext = $securityContext;
+        $this->publishWorkflowChecker = $publishWorkflowChecker;
+        $this->publishWorkflowPermission = $attribute;
     }
 
+    /**
+     * Handle the event.
+     *
+     * @param CreateMenuItemFromNodeEvent $event
+     */
     public function onCreateMenuItemFromNode(CreateMenuItemFromNodeEvent $event)
     {
         $node = $event->getNode();
-        $item = $event->getItem();
-        $factory = $event->getFactory();
 
-        if ($node instanceof Menu) {
-            return;
+        if (!$this->publishWorkflowChecker->isGranted($this->publishWorkflowPermission, $node)) {
+            if ($node instanceof Menu) {
+                $event->setItem($event->getFactory()->createItem($node->getName()));
+            } else {
+                $event->setSkipNode(true);
+            }
         }
-
-        if (!$this->securityContext->isGranted($this->publishWorkflowPermission, $node)) {
-            $event->setSkipNode(true);
-        }
-
     }
 }
 
